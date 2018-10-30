99eb361eebd843a3b59290009a7a688a9bfe800c
GriZmio <grizmio@gmail.com>
2018-10-30 11:03:33 -0300

    Fue removido el filtrado por fecha al solicitar el estado de un trámite por su numero, esto corrige un error en mysql ya que la query dependia de poder usar fechas con 00 y ademas el filtrado no era necesario ni util.

--- app/Http/Controllers/ConsultController.php
+++ app/Http/Controllers/ConsultController.php
@@ -15,31 +15,26 @@ class ConsultController extends Controller
         $data['titulo'] = 'Seguimiento de Trámites en Línea';
         $resp = '<br/><div class="alert alert-warning"><strong>Sin datos Disponibles</strong></div>';
 
-        $fecha = $request->input('fecha', old('fecha'));
         $nrotramite = trim($request->input('nrotramite', old('nrotramite')));
 
         if ($request->isMethod('post')) {
-            $fecha = $request->input('fecha');
             $nrotramite = trim($request->input('nrotramite'));
 
             $request->validate([
-                'fecha' => 'required|digits_between:1,30|numeric',
                 'nrotramite' => 'required|digits_between:1,30|numeric'
             ], [
                 'nrotramite.numeric' => 'El campo <b>Nro. de Trámite</b> debe ser un número.',
                 'nrotramite.required' => 'El campo <b>Nro. de Trámite</b> es obligatorio.',
-                'fecha.numeric' => 'El campo <b>Fecha</b> debe ser un número.',
-                'fecha.required' => 'El campo <b>Fecha</b> es obligatorio.'
+                'digits_between' => 'El campo <b>Nro. de Trámite</b> debe contener entre :min y :max dígitos.'
             ]);
 
-            if (is_numeric($nrotramite) && is_numeric($fecha)) {
-                $query = (new \Consultas)->listDatoSeguimiento($nrotramite, $fecha, \Cuenta::cuentaSegunDominio());
+            if (is_numeric($nrotramite)) {
+                $query = (new \Consultas)->listDatoSeguimiento($nrotramite, \Cuenta::cuentaSegunDominio());
                 $data['vacio'] = $resp;
             }
 
         };
 
-        $data['fecha'] = $fecha;
         $data['nrotramite'] = $nrotramite;
         $data['tareas'] = $query;
         $data['content'] = view('consult.index', $data);
--- app/Models/Doctrine/consultas.php
+++ app/Models/Doctrine/consultas.php
@@ -9,7 +9,7 @@ class Consultas extends Doctrine_Record
         parent::__construct();
     }
 
-    public function listDatoSeguimiento($tra_id, $fecha, $cuenta)
+    public function listDatoSeguimiento($tra_id, $cuenta)
     {
         $statement = Doctrine_Manager::getInstance()->connection();
         $query = "select 
@@ -20,19 +20,16 @@ class Consultas extends Doctrine_Record
                     tarea_id, 
                     tarea_nombre, 
                     usuario, 
-                    inicio_filtro,
                     etapa_id
                  from (
                        select 
                        t.id as nro_tramite, 
-                       (CASE WHEN LENGTH(t.id) = 6 THEN t.id - CONCAT(SUBSTRING('$fecha',3,4),'0000') ELSE t.id  END ) AS filtro, 
                        DATE_FORMAT(e.created_at,'%d-%m-%Y %H:%i:%s') as inicio, 
                        DATE_FORMAT(e.ended_at,'%d-%m-%Y %H:%i:%s') as termino, 
                        case when e.pendiente = 0 then 'Completado' else 'Pendiente' end as estado, 
                        ta.id as tarea_id, 
                        ta.nombre as tarea_nombre, 
                        concat(u.nombres,' ',u.apellido_paterno) as usuario,
-                       DATE_FORMAT(t.created_at,'%Y') as inicio_filtro,
                        e.id as etapa_id
                     from etapa e,tarea ta,tramite t, usuario u, proceso p, cuenta c
                     where e.tarea_id=ta.id 
@@ -43,8 +40,8 @@ class Consultas extends Doctrine_Record
                       and p.cuenta_id=c.id
                       and c.id=$cuenta->id                    
                     ) d
-                where nro_tramite=$tra_id and inicio_filtro=DATE_FORMAT(STR_TO_DATE('$fecha','%Y'),'%Y')
-                order by 4,2 asc";
+                where nro_tramite=$tra_id 
+                order by estado,inicio asc";
         $results = $statement->execute($query);
         return $results->fetchAll();
     }
