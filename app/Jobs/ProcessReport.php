1277832eba24bb4ed33a4f1adaf79d425ce2d158
Manuel Muñoz <manuel.munoz00@gmail.com>
2019-02-15 10:55:59 -0300

    se agrega job para optimizar reportes backend, se modifica gitignore y se comenta secciones agenda

---
+++ app/Jobs/ProcessReport.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Job;
+use App\Models\Reporte;
+use Cuenta;
+use App\Helpers\Doctrine;
+use Doctrine_Query;
+use DB;
+use Carbon\Carbon;
+
+class ProcessReport implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user_id;
+    protected $user_type;
+    protected $proceso_id;
+    protected $reporte_id;
+    protected $params;
+    protected $max_running_jobs = 1;
+    protected $tries = 1;
+    protected $job_info;
+    protected $reporte_tabla;
+    protected $header_variables;
+    protected $link_host;
+    protected $email_to;
+    protected $email_subject;
+    protected $email_message;
+    protected $email_name;
+    protected $_base_dir;
+    protected $nombre_reporte;
+    protected $desde;
+    protected $hasta;
+    protected $pendiente;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($user_id,$user_type,$proceso_id,$reporte_id,$params,$reporte_tabla,$header_variables,$host, $email_to, $email_name, $email_subject, $desde, $hasta, $pendiente)
+    {
+        $this->user_id = $user_id;
+        $this->user_type = $user_type;
+        $this->proceso_id = $proceso_id;
+        $this->reporte_id = $reporte_id;
+        $this->params = $params;
+        $this->reporte_tabla = $reporte_tabla;
+        $this->header_variables = $header_variables;
+        $this->link_host = $host;
+        $this->email_to = $email_to;
+        $this->email_name = $email_name;
+        $this->email_subject = $email_subject;
+        $this->_base_dir = public_path('uploads/tmp');
+        if(!file_exists($this->_base_dir) ) {
+            mkdir($this->_base_dir, 0777, true);
+        }
+        $this->desde = $desde;
+        $this->hasta = $hasta;
+        $this->pendiente = $pendiente;
+        
+        $this->job_info = new Job();
+        $this->arguments = serialize([$user_id, $user_type, $proceso_id, $reporte_id]);
+        $this->job_info->user_id = $this->user_id;
+        $this->job_info->user_type = $this->user_type;
+        $this->job_info->arguments = $this->arguments;
+        $this->job_info->status = Job::$created;
+        $this->job_info->save();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {   
+        $this->job_info->status = Job::$running;
+        $this->job_info->save();
+
+        $this->generar_consulta();
+
+        $this->job_info->status = Job::$finished;
+        $this->job_info->filename = $this->nombre_reporte.'.xls';
+        $this->job_info->filepath = $this->_base_dir;
+        $this->job_info->save();
+        $this->send_notification();
+    }
+
+    private function generar_consulta(){
+
+        $excel_row = $this->reporte_tabla;
+        $header_variables = $this->header_variables;
+
+        $query = DB::table('tramite')
+            ->join('etapa', 'tramite.id', '=', 'etapa.tramite_id')
+            ->join('dato_seguimiento', 'dato_seguimiento.etapa_id', '=', 'etapa.id')
+            ->join('proceso', 'proceso.id', '=', 'tramite.proceso_id')
+            ->select(DB::raw('tramite.id as id, proceso.id as proceso_id, tramite.created_at as created_at'))
+            ->where('proceso.id',$this->proceso_id);
+
+
+        if(!is_null($this->desde)){
+            $this->desde = $this->desde.'00:00:00';
+            $this->desde = date('Y-m-d H:i:s', strtotime($this->desde));
+            $desde = Carbon::createFromFormat('Y-m-d H:i:s',$this->desde);
+            $query = $query->where('tramite.created_at','>=',$desde);
+        }
+            
+        if(!is_null($this->hasta)){
+            $this->hasta = $this->hasta.'23:59:59';
+            $this->hasta = date('Y-m-d H:i:s', strtotime($this->hasta));
+            $hasta = Carbon::createFromFormat('Y-m-d H:i:s',$this->hasta);
+            $query = $query->where('tramite.created_at','<=',$hasta);
+        }
+
+        if($this->pendiente != -1)
+            $query = $query->where('tramite.pendiente',$this->pendiente);
+
+        $query = $query
+            ->groupBy('tramite.id')
+            ->havingRaw('COUNT(dato_seguimiento.id) > 0 OR COUNT(etapa.id) > 1')
+            ->get();
+
+        $tramites = json_decode(json_encode($query), true);
+
+        foreach ($tramites as $t) {
+            $row = array();
+
+            $datos_actuales = DB::table('tramite')
+                ->join('etapa', 'tramite.id', '=', 'etapa.tramite_id')
+                ->join('dato_seguimiento', 'dato_seguimiento.etapa_id', '=', 'etapa.id')
+                ->select(DB::raw('dato_seguimiento.id, MAX(dato_seguimiento.id) as max_id'))
+                ->where('tramite.id',$t['id'])
+                ->groupBy('dato_seguimiento.nombre')
+                ->get();
+
+            $datos_actuales_ids=array();
+            foreach($datos_actuales as $d)
+                $datos_actuales_ids[]=$d->max_id;
+
+            $datos = DB::table('tramite')
+                ->join('etapa', 'tramite.id', '=', 'etapa.tramite_id')
+                ->join('dato_seguimiento', 'dato_seguimiento.etapa_id', '=', 'etapa.id')
+                ->whereIn('dato_seguimiento.id',$datos_actuales_ids)
+                ->groupBy('dato_seguimiento.nombre')
+                ->get();
+
+
+            foreach ($datos as $d) {
+                $val = $d->valor;
+                if (!is_string($val)) {
+                    $val = json_encode($val, JSON_UNESCAPED_UNICODE);
+                }
+                $t[$d->nombre] = strip_tags($val);
+            }
+
+            foreach ($header_variables as $h) {
+                $var_find = explode("->", $h);
+                if (count($var_find) > 1) {
+                    $row[] = isSet($t[$var_find[0]]) ? json_decode($t[$var_find[0]])->$var_find[1] : '';
+                } else {
+                    $row[] = isSet($t[$h]) ? $t[$h] : '';
+                }
+            }
+            $excel_row[] = $row;
+        }
+
+        $this->nombre_reporte = 'reporte-'.$this->reporte_id.'-'.Carbon::now('America/Santiago')->format('dmYHis');
+        Excel::create($this->nombre_reporte, function ($excel) use ($excel_row) {
+            $excel->sheet('reporte', function ($sheet) use ($excel_row) {
+                $sheet->fromArray($excel_row, null, 'A1', false, false);
+            });
+        })->store('xls', $this->_base_dir);  
+    }
+
+    private function send_notification(){
+        $link = "{$this->link_host}/backend/reportes/descargar_archivo/{$this->user_id}/{$this->job_info->id}/{$this->job_info->filename}";
+        $data = ['link' => $link];
+        $cuenta = Cuenta::cuentaSegunDominio();
+        $email_to = $this->email_to;
+        $email_subject = $this->email_subject;
+        Mail::send('emails.download_link', $data, function($message) use ($cuenta, $link, $email_to, $email_subject){
+
+            $message->subject($email_subject);
+            $mail_from = env('MAIL_FROM_ADDRESS');
+            if(empty($mail_from))
+                $message->from($cuenta->nombre . '@' . env('APP_MAIN_DOMAIN', 'localhost'), $cuenta->nombre_largo);
+            else
+                $message->from($mail_from);
+
+            $message->to($email_to);
+        });
+    }
+
+    
+}
