dee24c90cff92325ff00c25d3b9a397464fe602b
Oscar Cardenas <ocardenas@digital.gob.cl>
2019-04-26 11:50:36 -0400

    Se agrego mensaje de error a la Regla GrillaDatosExternos

--- app/Rules/GrillaDatosExternos.php
+++ app/Rules/GrillaDatosExternos.php
@@ -55,6 +55,6 @@ class GrillaDatosExternos implements Rule
      */
     public function message()
     {
-        return 'The validation error message.';
+        return trans('validation.valid_grilla');
     }
 }
