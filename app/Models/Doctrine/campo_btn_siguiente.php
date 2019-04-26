0e7962919d35f0ba2c5e07447e6e56796f67e895
Manuel Muñoz <mmunoz@digital.gob.cl>
2019-04-26 15:41:37 -0400

    correcciones campo botón siguiente

--- app/Models/Doctrine/campo_btn_siguiente.php
+++ app/Models/Doctrine/campo_btn_siguiente.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class CampoBtnSiguiente extends Campo
 {
-
     public $requiere_datos = false;
     public $estatico = true;
 
@@ -20,9 +19,8 @@ class CampoBtnSiguiente extends Campo
             $valor_default = $this->valor_default;
         }
 
-        $display = '<div id="div_btn_siguiente" class="form-group float-left">';
-        $display .= '<button id="' . $this->id . '" ' . ($modo == 'visualizacion' ? 'readonly' : '') . ' type="submit" class="btn btn-simple btn-danger" name="' . $this->nombre . '" value="' . ($dato ? htmlspecialchars($dato->valor) : htmlspecialchars($valor_default)) . '" data-modo="' . $modo . '" > ' . $this->etiqueta . '</button>';
-        $display .= '</div><br>';
+        $display = '<button id="' . $this->id . '" ' . ($modo == 'visualizacion' ? 'readonly' : '') . ' type="submit" class="btn btn-simple btn-danger" name="' . $this->nombre . '" value="' . ($dato ? htmlspecialchars($dato->valor) : htmlspecialchars($valor_default)) . '" data-modo="' . $modo . '" > ' . $this->etiqueta . '</button>';
+        $display .= '<br>';
 
         return $display;
     }
