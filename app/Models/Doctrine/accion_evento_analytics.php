0b5417cee4f7e85299cf91dbb2a2b86b71659bd6
jparada <jparada@digital.gob.cl>
2019-08-07 19:09:25 -0400

    Modificación de span dinamico en event

--- app/Models/Doctrine/accion_evento_analytics.php
+++ app/Models/Doctrine/accion_evento_analytics.php
@@ -38,29 +38,13 @@ class AccionEventoAnalytics extends Accion
         $display .= '<input type="hidden" name="extra[id_seguimiento]" id="id_seguimiento" value="'. (isset($this->extra->id_seguimiento) ? $this->extra->id_seguimiento : '') .'"></input>';            
             
         $display .= '<br><label title="Para Chile corresponde: Marca Inicial, Ingreso Solicitud, Marca Final">EventAction <br>  Nota: Para Chile corresponde: Marca Inicial, Ingreso Solicitud, Marca Final</label>';
-        $display .= '<input type="text" class="form-control col-2" id="nombre_marca" name="extra[nombre_marca]" onkeyup=mostrar(this.value) value="' . (isset($this->extra->nombre_marca) ? $this->extra->nombre_marca : '') . '" />';            
+        $display .= '<input type="text" class="form-control col-2" id="nombre_marca" name="extra[nombre_marca]" onkeyup=mostrar(this.value) value="' . (isset($this->extra->nombre_marca) ? $this->extra->nombre_marca : '') . '">';            
        
         $display .= '<label title="Para Chile es Categoria Trámite Digital">EventCategory <br>  Nota: Para Chile es Categoria Trámite Digital</label>';
-        $display .= '<input type="text" class="form-control col-2" id="categoria" name="extra[categoria]" onkeyup=eventos(this.value) value="' . (isset($this->extra->categoria) ? $this->extra->categoria : '') . '" />';
+        $display .= '<input type="text" class="form-control col-2" id="categoria" name="extra[categoria]" onkeyup=eventos(this.value) value="' . (isset($this->extra->categoria) ? $this->extra->categoria : '') . '">';
         $display .= '<br><label title="Para Chile es el ID RNT">EventLabel <br> Nota: Para Chile es el ID RNT</label>';
-        $display .= '<input type="text" class="form-control col-2" name="extra[evento_enviante]"  onkeyup=evento(this.value) value="' . (isset($this->extra->evento_enviante) ? $this->extra->evento_enviante : '') . '" />';
+        $display .= '<input type="text" class="form-control col-2" name="extra[evento_enviante]"  onkeyup=evento(this.value) value="' . (isset($this->extra->evento_enviante) ? $this->extra->evento_enviante : '') . '">';
         $display .= '<br><label><b>JSON GA</b></label>';
-        $display .='
-                    <script type="text/javascript">
-                        $(document).ready(function(){ 
-                            console.log("hola");  
-                            $(".tipo-seguimiento").on("click", function () {              
-                                let id_seguimiento = "'.$id_instancia.'";
-                                
-                                if ($(this).val()== "id_cuenta") {
-                                    id_seguimiento = "'.$id_cuenta.'";
-                                }
-
-                                $("#id_seguimiento").val(id_seguimiento);
-                            });
-                        });
-                    </script>';   
-
         $display .= '<span><br>{<br><br>
                   hitType: "event",<br><br>
                   eventAction: <label id="resultado"></label>"'.(isset($this->extra->nombre_marca) ? $this->extra->nombre_marca : '').'", <br><br>
@@ -69,14 +53,7 @@ class AccionEventoAnalytics extends Accion
                   
                      }</span>';
 
-                     $display .= '<script type="text/javascript>
-                     function myFunction() {
-                       alert("I am an alert box!");
-                     }
-                     </script>';
-
                      $display.= '<script type="text/javascript">
-                    
                       function mostrar(valor)
                       {
                           document.getElementById("resultado").innerHTML=valor;
@@ -90,10 +67,26 @@ class AccionEventoAnalytics extends Accion
                       function evento(valor)
                       {
                           document.getElementById("evento").innerHTML=valor;
+                          
                       }
-                     
-                      </script><br>'
-                      ;
+                      </script><br>';
+
+        $display .='
+                    <script type="text/javascript">
+                        $(document).ready(function(){  
+                            $(".tipo-seguimiento").on("click", function () {              
+                                let id_seguimiento = "'.$id_instancia.'";
+                                
+                                if ($(this).val()== "id_cuenta") {
+                                    id_seguimiento = "'.$id_cuenta.'";
+                                }
+
+                                $("#id_seguimiento").val(id_seguimiento);
+                            });
+                        });
+                    </script>';   
+
+        
 
                       } elseif (!empty($id_cuenta) && empty($id_instancia)) {
                           $display .='<label>Selecciona el ID a enviar</label><br/>'; 
