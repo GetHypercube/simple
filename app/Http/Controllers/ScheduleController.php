c571cdfd3218a7db26dd1c428764ae720cbe14fd
Manuel Muñoz <mmunoz@digital.gob.cl>
2019-11-08 10:31:19 -0300

    se modifica estructura de función de envío de correo

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -26,8 +26,7 @@ class ScheduleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index(){
         $etapas_avanzadas = \Artisan::call('simple:avanzar');
         $emails_enviados = \Artisan::call('simple:sendmails');
         $limpieza = \Artisan::call('simple:limpieza');
@@ -35,24 +34,25 @@ class ScheduleController extends Controller
         $cuenta = \Cuenta::cuentaSegunDominio();
         $to = env('DESTINATARIOS_CRON');
         $subject = 'cron de tareas';
+        $destinatarios = explode(",",$to);
         if(!empty($to)){
-            Mail::send('emails.send', ['content' => $message], function ($message) use ($subject, $cuenta, $to) {
-                $message->subject($subject);
-                $mail_from = env('MAIL_FROM_ADDRESS');
-                if(empty($mail_from)) {
-                    $message->from($cuenta->nombre . '@' . env('APP_MAIN_DOMAIN', 'localhost'), $cuenta->nombre_largo);
-                } else {
-                    $message->from($mail_from);
-                }
-
-                if (!is_null($to)) {
-                    foreach (explode(',', $to) as $to) {
-                        if (!empty($to)) {
-                            $message->to(trim($to));
-                        }
-                    }
-                }
-            });
+            foreach($destinatarios as $destinatario){
+                $this->envio_correo($destinatario,$message,$subject,$cuenta);
+            }
         }
+        return $message;
+    }
+
+    private function envio_correo($destinatario,$message,$subject,$cuenta){
+        Mail::send('emails.send', ['content' => $message], function ($message) use ($subject, $cuenta, $destinatario) {
+            $message->subject($subject);
+            $mail_from = env('MAIL_FROM_ADDRESS');
+            if(empty($mail_from)) {
+                $message->from($cuenta->nombre . '@' . env('APP_MAIN_DOMAIN', 'localhost'), $cuenta->nombre_largo);
+            } else {
+                $message->from($mail_from);
+            }
+            $message->to($destinatario);
+        });
     }
 }
