f7ecb2de92a2733e9406fabf989c8ab2987395fb
Aroldo <aroldo@digital.gob.cl>
2019-11-28 15:36:36 -0300

    ajuste en intall.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,4 @@
-docker-compose up --build && \
+docker-compose up --build -d && \
 echo "Please wait while service is up..." && \
 sleep 5 && \
 docker exec simple2_web bash /var/www/simple/setup/simple.sh && \
