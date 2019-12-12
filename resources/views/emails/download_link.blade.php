5a95ff129a32707a69d81c9e87f3e04661a46d98
jparada <jparada@digital.gob.cl>
2019-12-12 12:22:46 -0300

    Modificacion del svg presidencial de tu reporte esta listo

--- resources/views/emails/download_link.blade.php
+++ resources/views/emails/download_link.blade.php
@@ -11,7 +11,18 @@
        <div id="svg" style="text-align:center;">
           <svg width="620px" height="575px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <rect width="620" height="575" style="fill:rgb(0,0,0,0);stroke-width:;stroke:rgb(0,0,0); margin: 0 auto;padding-top: 50px;" />
-      <image xlink:href="{{ asset('img/reportes/presidential_bar.svg') }}" x=20 height="5px" width="118px"/>
+            <g id="mails-correos-automáticos" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g id="Mail-Retoma-de-Solicitud" transform="translate(-569.000000, -266.000000)" fill-rule="nonzero">
+                                <g id="presidential_bar" transform="translate(569.000000, 266.000000)">
+                                    <g id="rect-1-link" fill="#0F69B4">
+                                        <rect id="rect-1" x="20" y="0" width="44" height="5"></rect>
+                                    </g>
+                                    <g id="rect-2-link" transform="translate(43.000000, 0.000000)" fill="#EB3C46">
+                                        <rect id="rect-2" x="20" y="0" width="75" height="5"></rect>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>                   
       <text x=20 y=90 stroke="#373737" font-family="Roboto Slab" font-size="25px" > ¡Tú reporte esta listo!</text>
       <text x=20 y=140 font-family="Roboto, Sans Serif" font-size="16px">Hola, el reporte <?php echo $reportname; ?> que has solicitado se encuentra listo</text>
       <text x=20 y=160 font-family="Roboto, Sans Serif" font-size="16px">para que puedas descargarlo.</text>
