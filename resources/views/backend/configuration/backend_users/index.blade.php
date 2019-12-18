6f968181a3cd6d89d9df154f42dfda9cbc3c264d
Aroldo <aroldo@digital.gob.cl>
2019-12-18 17:22:46 -0300

    #133 - se ajusta búsqueda y ordenamiento en listado usuarios backend

--- resources/views/backend/configuration/backend_users/index.blade.php
+++ resources/views/backend/configuration/backend_users/index.blade.php
@@ -19,22 +19,76 @@
                     </ol>
                 </nav>
 
-                <div class="row">
-                    <div class="col-12">
-                        <a href="{{route('backend.configuration.backend_users.add')}}"
+                <div class="row align-items-center d-flex justify-content-between">
+                    <div class="col-2 mb-4 mt-2">
+                        <a href="{{ route('backend.configuration.backend_users.add') }}"
                            class="btn btn-success">
                             <i class="material-icons">note_add</i> Nuevo
                         </a>
                     </div>
-                    <br>
-                    <br>
+                    <div class="col-6 mb-4 mt-2">
+                        <form action="{{ route('backend.configuration.backend_users') }}" class="form">
+                           <div class="row justify-content-end">
+                               <div class="col-5">
+                                   <input type="text" class="form-control" placeholder="Escribe el email a buscar aquí"
+                                   name="email_search" value="{{ $email_search  }}">
+                               </div>
+                               <div class="col-2">
+                                   <button type="submit" class="btn btn-primary">
+                                       <i class="material-icons">search</i>
+                                       Buscar
+                                   </button>
+                               </div>
+                           </div>
+                        </form>
+                    </div>
                     <div class="col-12">
                         <table class="table">
                             <thead>
-                            <th>E-Mail</th>
-                            <th>Nombre</th>
-                            <th>Apellidos</th>
-                            <th>Rol</th>
+                            <th>
+                                E-Mail
+                                <a href="{{ route('backend.configuration.backend_users').'?order_by_email='.(null==$order_by_email ? 'asc':$order_by_email) }}" class="arrow-order">
+                                    <i class="material-icons top-arrow {{ ($order_by_email && $order_by_email == 'asc') ? 'active':'' }}">
+                                        arrow_drop_up
+                                    </i>
+                                    <i class="material-icons bottom-arrow {{ ($order_by_email && $order_by_email == 'desc') ? 'active':'' }}">
+                                        arrow_drop_down
+                                    </i>
+                                </a>
+                            </th>
+                            <th>
+                                Nombre
+                                <a href="{{ route('backend.configuration.backend_users').'?order_by_name='.(null==$order_by_name ? 'asc':$order_by_name) }}" class="arrow-order">
+                                    <i class="material-icons top-arrow {{ ($order_by_name && $order_by_name == 'asc') ? 'active':'' }}">
+                                        arrow_drop_up
+                                    </i>
+                                    <i class="material-icons bottom-arrow {{ ($order_by_name && $order_by_name == 'desc') ? 'active':'' }}">
+                                        arrow_drop_down
+                                    </i>
+                                </a>
+                            </th>
+                            <th>
+                                Apellidos
+                                <a href="{{ route('backend.configuration.backend_users').'?order_by_lastname='.(null==$order_by_lastname ? 'asc':$order_by_lastname)  }}" class="arrow-order">
+                                    <i class="material-icons top-arrow {{ ($order_by_lastname && $order_by_lastname == 'asc') ? 'active':'' }}">
+                                        arrow_drop_up
+                                    </i>
+                                    <i class="material-icons bottom-arrow {{ ($order_by_lastname && $order_by_lastname == 'desc') ? 'active':'' }}">
+                                        arrow_drop_down
+                                    </i>
+                                </a>
+                            </th>
+                            <th>
+                                Rol
+                                <a href="{{ route('backend.configuration.backend_users').'?order_by_rol='.(null==$order_by_rol ? 'asc':$order_by_rol)  }}" class="arrow-order">
+                                    <i class="material-icons top-arrow {{ ($order_by_rol && $order_by_rol == 'asc') ? 'active':'' }}">
+                                        arrow_drop_up
+                                    </i>
+                                    <i class="material-icons bottom-arrow {{ ($order_by_rol && $order_by_rol == 'desc') ? 'active':'' }}">
+                                        arrow_drop_down
+                                    </i>
+                                </a>
+                            </th>
                             <th>Acciones</th>
                             </thead>
                             <tbody>
