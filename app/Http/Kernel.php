651a38f094b0d1f33388184fe1d92d0a285f28be
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2020-02-26 22:25:58 -0600

    Updated Laravel from 5.5 to 5.6

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'auth_user' => \App\Http\Middleware\UserMiddleware::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
     ];
 }
